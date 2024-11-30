6c4bade54fef5fc7cfeb08b1497429123bb85932
vishal lokhande <lvishal108@gmail.com>
2024-11-30 13:15:12 +0000

    apply css classes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -15,16 +15,19 @@ const Counter = () => {
   }
 
   return (
-    <div className="flex items-center justify-center h-40 w-70 h-50 border-2 border-black rounded  mainbox center-div container mx-auto m-20 bg-pink-200">
+    
+    <div className="flex items-center justify-center h-40 w-70 h-50 border-4 border-black rounded  mainbox center-div container mx-auto m-20 bg-gradient-to-r from-green-500 via-yellow-500 to-pink-500 ...">
+
+      
       <button
-        className="bg-blue-500 text-white p-2 rounded-md mr-2 w-32 h-10"
+        className="bg-blue-500 text-white p-2 rounded-md mr-2 w-32 h-10 font-bold border-2 border-black"
         onClick={handleClick}
       >
         Increment
       </button>
-      <p>Count: {counter}</p>
+      <p className="text-3xl font-bold">Count : {counter}</p>
       <button
-        className="bg-blue-500 text-white p-2 rounded-md ml-2 w-32 h-10"
+        className="bg-blue-500 text-white p-2 rounded-md ml-2 w-32 h-10 font-bold border-2 border-black"
         onClick={handleDecrement}
       >
         Decrement
@@ -34,5 +37,3 @@ const Counter = () => {
 };
 
 export default Counter;
-
-
